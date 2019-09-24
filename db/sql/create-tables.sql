3e44b70536a759f6869771a208373da3be97a43b
Yuta <uc4w6c@bma.biglobe.ne.jp>
2019-09-24 22:21:43 +0900

    lock制約を作るためのテーブル作成

--- db/sql/create-tables.sql
+++ db/sql/create-tables.sql
@@ -14,7 +14,17 @@ create table testdb.password_reissue_info
  email                  varchar(50) not null
  , token                varchar(250) not null
  , expiry_date          datetime not null
- ,   primary key (email) 
+ ,   primary key (email)
+ ,   foreign key fk_email (email) references accounts(email)
+);
+
+create table testdb.login_failure_manager
+(
+ email                  varchar(50) not null
+ , failure_count        tinyint not null default 0
+ , lock_count           tinyint not null default 0
+ , last_fail_datetime   datetime
+ ,   primary key (email)
  ,   foreign key fk_email (email) references accounts(email)
 );
 
